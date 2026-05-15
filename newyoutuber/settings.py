7ee777a5056ad29a6ff9892c942e410a7e7d0995
Rishi Ram Bhusal <risihirambhusal444@gmail.com>
2026-05-15 15:53:47 +0545

    allow the hosting to onrender

--- newyoutuber/settings.py
+++ newyoutuber/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -139,6 +140,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = 'subscribers:login'
 LOGIN_REDIRECT_URL = 'subscribers:dashboard'
