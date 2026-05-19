60f6e1dc6ed1fa2223b18f6f501231f5287ebc95
Rishi Ram Bhusal <risihirambhusal444@gmail.com>
2026-05-19 11:43:31 +0545

    thiis is after using database postgrade

--- newyoutuber/settings.py
+++ newyoutuber/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Load environment variables from .env file
 load_dotenv()
@@ -93,10 +94,11 @@ WSGI_APPLICATION = 'newyoutuber.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
@@ -173,7 +175,12 @@ TASK_ACTIVITY_WINDOW_MINUTES = int(os.environ.get("TASK_ACTIVITY_WINDOW_MINUTES"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'subscribers.User'
 
+allowed_hosts_raw = os.environ.get("DJANGO_ALLOWED_HOSTS", "")
 ALLOWED_HOSTS = [
+    host.strip()
+    for host in allowed_hosts_raw.split(",")
+    if host.strip()
+] or [
     "arkoboost.onrender.com",
     "localhost",
     "127.0.0.1",
